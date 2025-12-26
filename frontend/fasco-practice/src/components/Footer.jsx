694cf5090c00d753cdc15e1156f225f68dc6b6c7
cerpa <cerpacamilo3@gmail.com>
2025-12-26 00:21:59 -0500

    Add footer component to MainLayout

--- frontend/fasco-practice/src/components/Footer.jsx
+++ frontend/fasco-practice/src/components/Footer.jsx
@@ -1,9 +1,24 @@
-
+import '../styles/Footer.css'
+import { Link } from 'react-router';
 
 const Footer = () => {
     return (
         <>
-        
+            <section className="footer-container">
+                <div className='footer-link-container'>
+                    <h1 className="footer-logo-fasco"> FASCO</h1>
+                    <ul className="footer-ul-container">
+                        <li className="footer-li"><a href=""></a>Support Center</li>
+                        <li className="footer-li"><a href=""></a>Invoicing</li>
+                        <li className="footer-li"><a href=""></a>Contract</li>
+                        <li className="footer-li"><a href=""></a>Carrers</li>
+                        <li className="footer-li"><a href=""></a>Blog</li>
+                        <li className="footer-li"><a href=""></a>FAQ,s</li>
+                    </ul>
+                </div>
+                <p className="footer-copyright">Copyright © 2025. All Rights Reseved. Built by <Link className='footer-link-copyright'> 
+                Camilo cer</Link> and designed by <Link className='footer-link-copyright'> hamza anza</Link></p>
+            </section>
         </>
     )
 }
